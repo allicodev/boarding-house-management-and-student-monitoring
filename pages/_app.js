087edf32312c418af088d847a3b2bf215a78c46a
Alley Shawwn <cayetuna2000@gmail.com>
2023-08-04 01:15:03 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -6,13 +6,7 @@ import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ConfigProvider
-    // theme={{
-    //   token: {
-    //     colorPrimary: "#00b96b",
-    //   },
-    // }}
-    >
+    <ConfigProvider>
       <Head>
         {/* <link rel='shortcut icon' href='/logo-icon.svg' /> */}
         <title>
